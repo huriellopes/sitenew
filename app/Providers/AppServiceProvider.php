ce9e97b084d14c341dcb744dd002abe5fe194b72
Huriel Lopes <huriellopes1996@gmail.com>
2019-12-16 19:45:12 -0300

    Styles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -104,6 +104,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env("APP_ENV") == "production") {
+            \URL::forceScheme('https');
+        }
     }
 }
--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="autor" content="Huriel Lopes" />
+    <meta name="keywords" content="sites, sistemas, web, mobile, desenvolvimento, consultoria" />
+    <meta name="description" content="Desenvolvimento de sites web, sistemas web e aplicativos mobile, consultorias na área de TI online." />
     <title>Huriel Lopes</title>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
     @yield('style')
@@ -14,9 +16,9 @@
 
     @yield('content')
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     @yield('script')
 </body>
 </html>
